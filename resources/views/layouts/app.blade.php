453f8b1dc421806f8752125eed388e4ae75f06e1
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2025-04-09 22:39:09 +0200

    suppression lien tableau de bord

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,9 +43,6 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('dashboard') }}">Tableau de bord</a>
-                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
