221a56d374612bb8f09663a0b63c9780717c0a07
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2025-04-09 22:42:31 +0200

    modif model

--- app/Models/ProjectMedia.php
+++ app/Models/ProjectMedia.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProjectMedia extends Model
 {
+    use HasFactory;
+
+    /**
+     * Les attributs qui sont mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'project_id',
+        'file_path',
+        'file_type',
+        'is_main',
+        'order',
+    ];
+
+    /**
+     * Relation avec le projet
+     */
     public function project()
     {
         return $this->belongsTo(Project::class);
