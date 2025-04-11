820a535fa9785e4ffe480c0aeb288cfa2394cdc2
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2025-04-11 16:40:01 +0200

    suppression toast

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,18 +86,6 @@
 
     <main class="py-4">
         <div class="container">
-            @if (session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-            @endif
-
-            @if (session('error'))
-                <div class="alert alert-danger">
-                    {{ session('error') }}
-                </div>
-            @endif
-
             @yield('content')
         </div>
     </main>
